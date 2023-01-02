5267fb46fe39624ff089346d10dad229212982db
Martin Polese <mpolese@fi.uba.ar>
2023-01-02 18:31:05 -0300

    Agrego notificacion de success con toastr al enviar el formulario de contacto

---
+++ scripts/contacto_page.js
@@ -0,0 +1,35 @@
+
+function mostrarMensajeDeEnviado(e) {
+  e.preventDefault();
+
+  toastr.options = {
+    "closeButton": false,
+    "debug": false,
+    "newestOnTop": true,
+    "progressBar": true,
+    "positionClass": "toast-bottom-right",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "2000",
+    "hideDuration": "0",
+    "timeOut": "2000",
+    "extendedTimeOut": "0",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+  }
+
+  toastr.success("Su datos seran enviados a la brevedad")
+
+  setTimeout(() => e.target.submit(), 2000);
+
+}
+
+function main() {
+  let form = document.getElementById("contacto-form");
+
+  form.addEventListener("submit", (e) => mostrarMensajeDeEnviado(e));
+}
+
+main();
